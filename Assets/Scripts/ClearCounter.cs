842d1f0af1d6009ac488f3895ff8cb384f313bb2
oleg.cherevko <cherevko.mail@gmail.com>
2024-07-29 17:36:18 +0300

    Implements the ability for kitchen object and counter to know about each other

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,9 +7,53 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private KitchenObjectSO kitchenObject;
     [SerializeField] private GameObject topCounterPoint;
 
+    [SerializeField] bool testing;
+    [SerializeField] ClearCounter secondClearCounter;
+
+    private KitchenObject placedObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T)) {
+            Debug.Log("Testing mode");
+            placedObject.SetClearCounter(secondClearCounter);
+        }
+    }
+
     public void Interact() {
-        GameObject kitchenObjectInstance = Instantiate(kitchenObject.prefab, topCounterPoint.transform);
-        kitchenObjectInstance.transform.localPosition = Vector3.zero;
-        Debug.Log(kitchenObjectInstance.transform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+        if (placedObject == null) {
+            // instantiate product
+            GameObject kitchenObjectInstance = Instantiate(kitchenObject.prefab, topCounterPoint.transform);
+            kitchenObjectInstance.GetComponent<KitchenObject>().SetClearCounter(this);
+            //// fix the position (TODO understand why it is not 0 by default)
+            //kitchenObjectInstance.transform.localPosition = Vector3.zero;
+
+            //// set placed object
+            //placedObject = kitchenObjectInstance.GetComponent<KitchenObject>();
+
+            //placedObject.SetClearCounter(this);
+        } else {
+            Debug.Log("Placed on " + placedObject.GetClearCounter());
+        }
+    }
+
+    public GameObject GetPlacementPoint() {
+        return this.topCounterPoint;
+    }
+
+    public void SetPlacedObject(KitchenObject kitchenObject) {
+        this.placedObject = kitchenObject;
+    }
+
+    public KitchenObject GetPlacedObject() {
+        return placedObject;
+    }
+
+    public void ClearPlacedObject() {
+        this.placedObject = null;
+    }
+
+    public bool HasPlacedObject() {
+        return placedObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,7 +6,30 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter newCounter) {
+        if (this.clearCounter != null) {
+            this.clearCounter.ClearPlacedObject();
+        }
+
+        if (newCounter.HasPlacedObject()) {
+            Debug.LogWarning("New counter already has kitchen object");
+            return;
+        }
+
+        this.clearCounter = newCounter;
+        this.clearCounter.SetPlacedObject(this);
+
+        transform.parent = newCounter.GetPlacementPoint().transform;
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() {
+        return this.clearCounter;
     }
 }
